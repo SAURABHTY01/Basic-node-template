7bd3d925815554d7a735cb17bb281e423d3eac3a
Saurabh Tyagi <saurabh.tyagi@quantumsystem.in>
2025-04-14 08:27:05 +0530

    home route added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.get("/", async (req, res) => {
+    console.log("app is running")
+    res.send("app is running")
+})
+
 app.listen(4040, () => console.log('server is running on 4040'))
